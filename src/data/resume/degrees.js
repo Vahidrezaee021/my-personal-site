9a1403a336fd9c6bbfd3fae67abc3246c855dba0
vahidrezaee <vahidrezai@ymail.com>
2025-04-23 13:57:59 +0330

    education

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,15 +1,21 @@
 const degrees = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
-    year: 2016,
+    school: 'Shiraz University',
+    degree: 'B.S. Computer Engineering',
+    link: 'https://shirazu.ac.ir/',
+    year: 2018,
   },
   {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
+    school: 'Soore International University',
+    degree: 'B.S. Drama and Literature',
+    link: 'https://soore.ac.ir/',
+    year: 2015,
+  },
+  {
+    school: 'Shahrood University of Technology',
+    degree: 'B.S. Mechanical Engineering',
+    link: 'https://www.shahroodut.ac.ir/',
+    year: 2010,
   },
 ];
 
